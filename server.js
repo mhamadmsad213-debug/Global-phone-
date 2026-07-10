98b8ad56ca4ab77c5b230f039873516384594b56
mhamadmsad213-debug <mhamadmsad213@gmail.com>
2026-07-10 20:46:57 +0300

    Modify server to serve index.html and change port
    Updated server to serve index.html and changed port to 10000.

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
